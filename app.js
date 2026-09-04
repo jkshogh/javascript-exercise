82b6d4bd67054d64647c84d00e91afa2514efce4
jkshogh <149129730+jkshogh@users.noreply.github.com>
2026-09-04 15:21:36 +0800

    feat: add function fizzBuzz (#10)
    * feat: add function fizzBuzz
    
    * feat: edit function fizzBuzz

--- app.js
+++ app.js
@@ -95,3 +95,41 @@ function mirrorStringv1(wording) {
         return false;
     }
 }
+
+//FizzBuzz
+
+function fizzBuzz (a,b) {
+    let stringLength = b - a + 1
+    let result = "" * 1
+
+    for (let i = 0; i < stringLength; i++){
+        result = result + 1;
+            if  (result % 3 == 0 & result % 5 == 0){
+                console.log ("FizzBuzz");
+            } else if (result % 3 == 0){
+                console.log ("Fizz");
+            } else if (result % 5 == 0){
+                console.log ("Buzz");
+            } else {
+                console.log(result)
+            }               
+    };      
+            
+}
+
+
+function fizzBuzz () {
+    for (let i = 1; i <= 100; i++){
+            if  (i % 3 == 0 & i % 5 == 0){
+                console.log ("FizzBuzz");
+            } else if (i % 3 == 0){
+                console.log ("Fizz");
+            } else if (i % 5 == 0){
+                console.log ("Buzz");
+            } else {
+                console.log(i)
+            }               
+    };      
+            
+}
+
